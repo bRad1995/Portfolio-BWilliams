d979bbcdf2c6c47b5246820a138f2ee1560b07aa
bRad1995 <bradleejwilliams@yahoo.com>
2023-12-19 20:11:19 -0600

    react router established, scoping for comps and pages done, App.js imports and URLs added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header.js"
+import Footer from './components/Footer.js';
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home.js"
+import About from "./pages/About.js"
+import Projects from './pages/Projects.js';
+
 
 function App() {
+  const URL = process.env.REACT_APP_URL || "http://localhost:4000";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects URL={URL}/>} />
+        <Route path="/about" element={<About URL={URL} />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
